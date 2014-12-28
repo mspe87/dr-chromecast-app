3dd3767062efa715ad605c3cf7143f67bc6513d8
Mads Petersen <mads26330847@gmail.com>
2014-12-28 20:14:16 +0100

    chrome extension

--- app/js/cast.js
+++ app/js/cast.js
@@ -38,24 +38,48 @@ function onRequestSessionSuccess(e){
 	session = e;
 	console.log("got a session " + e);
 
-	var mediaInfo = new chrome.cast.media.MediaInfo("http://dr04-lh.akamaihd.net/i/dr04_0@147057/master.m3u8?b=100-1600");
+
+
+	var danishSubtitle = new chrome.cast.media.Track(1, // track ID
+		chrome.cast.media.TrackType.TEXT);
+	danishSubtitle.trackContentId = 'https://www.dr.dk/mu-online/api/1.2/bar/548062936187a20d7cf48704';
+	danishSubtitle.contentType = 'text/vtt';
+	danishSubtitle.subtype = chrome.cast.media.TextTrackType.SUBTITLES;
+	danishSubtitle.name = 'Danish Subtitles';
+	danishSubtitle.language = 'da';
+	danishSubtitle.customData = null;
+	var tracks = [danishSubtitle];
+
+
+	var mediaInfo = new chrome.cast.media.MediaInfo("http://drod09k-vh.akamaihd.net/i/all/clear/streaming/d3/549ca479a11f9d1b0c32acd3/Hele-Danmarks-X-Factor_6d413d9a964941028dcadcb14e403a08_,1126,562,248,.mp4.csmil/master.m3u8");
 
 	mediaInfo.metadata = new chrome.cast.media.GenericMediaMetadata();
 	mediaInfo.metadata.metadataType = chrome.cast.media.MetadataType.GENERIC;
 	mediaInfo.contentType = 'video/mp4';
+	mediaInfo.streamType = chrome.cast.media.StreamType.BUFFERED;
+	mediaInfo.metadata.title = "Arvingerne (10:10)";
+	mediaInfo.metadata.images = [{'url': "http://www.dr.dk/mu-online/api/1.2/bar/530b09bca11f9d08c8cc3d4c"}];
+	
+	mediaInfo.textTrackStyle = new chrome.cast.media.TextTrackStyle();
+	mediaInfo.duration = null;
+	mediaInfo.tracks = tracks;
 
-	mediaInfo.metadata.title = "test";
-	mediaInfo.metadata.images = [{'url': "http://img2.wikia.nocookie.net/__cb20140523031547/american-revolutionary-war/images/e/ec/Ok-icon.png"}];
+	console.log(mediaInfo);
+
 	var request = new chrome.cast.media.LoadRequest(mediaInfo);
 	request.autoplay = true;
 	request.currentTime = 0;
+	request.activeTrackIds=[1];
 
 	session.loadMedia(request,
 		onMediaDiscovered.bind(this, 'loadMedia'),
 		onMediaError);
+
+
+
 }
 function onLaunchError(){
-	alert("sa");
+	console.log("sa");
 }
 
 function onMediaError(){
